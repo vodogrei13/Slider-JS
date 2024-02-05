82dd7af6b2dcee52b7c364d5e01a52b7b28713d6
vodogrei13 <vodogrei15@mail.ru>
2024-02-05 14:00:24 +0300

    five commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Массив Картинок
 let images = [{
     url: "./images/png/img-one.png",
   }, {
@@ -5,10 +6,11 @@ let images = [{
   }, {
     url: "./images/png/img-three.png",
   }];
+  
+//Массивы Текстов
+  let paragraph1 = ["<span>Rostov-on-Don<br>LCD admiral</span>", "<span>Sochi<br>Thieves</span>","<span>Rostov-on-Don<br>Patriotic</span>"];
 
-  let paragraph1 = ["Rostov-on-Don LCD admiral", "Sochi Thieves","Rostov-on-Don Patriotic"];
-
-  let paragraph2 = ["81 m2", "105 m2", "Rostov-on-Don Patriotic"];
+  let paragraph2 = ["81 m2", "105 m2", "93 m2"];
 
   let paragraph3 = ["3.5 months", "4 months", "3 months"];
 
@@ -29,17 +31,17 @@ function initSlider(images, options) {
     if (options.dots) {
         initDots();
     }
-
+//Функция слайдер-картинок
     function initImages() {
         images.forEach((image, index) => {
             let imageElement = document.createElement("div");
-            imageElement.className = `image n${index} ${index? "" : "active"}`;
+            imageElement.className = `image n${index} ${index ? "" : "active"}`;
             imageElement.dataset.index = index;
             imageElement.style.backgroundImage = `url(${image.url})`;
             sliderImages.appendChild(imageElement);
         });
     }
-
+//Функция слайдер-стрелки
     function initArrows() {
         let lastIndex = images.length - 1;
         sliderArrows.querySelectorAll('.block-arrow-item').forEach(arrow => {
@@ -55,7 +57,22 @@ function initSlider(images, options) {
             });
         });
     }
-
+//Функция слайдер-точки
+    function initDots() {
+        let dotsWrapper = document.createElement("div");
+        dotsWrapper.className = "slider__dots"; 
+        images.forEach((_, index) => {
+          let dot = document.createElement("div");
+          dot.className = `slider__dots-item n${index} ${index ? "" : "active"}`;
+          dot.dataset.index = index;
+          dot.addEventListener("click", function() {
+            moveSlider(this.dataset.index);
+          });
+          dotsWrapper.appendChild(dot);
+        });
+        document.querySelector('.block-point').appendChild(dotsWrapper);
+      }
+//Функция изменения классов в зависимости от Активности
     function moveSlider(num) {
         sliderImages.querySelector(".active").classList.remove("active");
         sliderImages.querySelector(`.n${num}`).classList.add("active");
@@ -65,23 +82,23 @@ function initSlider(images, options) {
             dotsWrapper.querySelector(".active").classList.remove("active");
             dotsWrapper.querySelector(`.n${num}`).classList.add("active");
           }
+        document.getElementById('city__text').innerHTML = paragraph1[num];
+        document.getElementById('apartment__text').innerText = paragraph2[num];
+        document.getElementById('repair__text').innerText = paragraph3[num];
+
+        document.querySelector('.address_active').classList.remove("address_active");
+        document.getElementById(`city__link_${num}`).classList.add('address_active');
     }
-    
-    function initDots() {
-        let dotsWrapper = document.createElement("div");
-        dotsWrapper.className = "slider__dots";
-        images.forEach((image, index) => {
-          let dot = document.createElement("div");
-          dot.className = `slider__dots-item n${index} ${index? "" : "active"}`;
-          dot.dataset.index = index;
-          dot.addEventListener("click", function() {
-            moveSlider(this.dataset.index);
-          });
-          dotsWrapper.appendChild(dot);
-        });
-        document.querySelector('.block-point').appendChild(dotsWrapper);
-      }
+//Ссылки сверху слайдера (включают нужный слайд)
+    document.getElementById('links__block').addEventListener('click', function(event) {
+        event.preventDefault();
+        const target = event.target
+        if(target.hasAttribute('data-index')) {
+            moveSlider(target.getAttribute('data-index'));
+        }
+    })
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
     let sliderOptions = {
